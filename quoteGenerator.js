0a6bbfe305b38e71927b5e08c319d8d9a7c07bf3
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2021-06-14 14:15:29 -0400

    ,

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,12 +1,12 @@
 var quotes = [
-    'Thank you for your kindness'
-    'You are so kind to think of me'
-    'I appreciate your interest'
-    'Thank you for showing up'
-    'Your words are meaningful'
-    'I hear what you are saying'
-    'I appreciate your time'
-    'Thank you for sharing'
+    'Thank you for your kindness',
+    'You are so kind to think of me',
+    'I appreciate your interest',
+    'Thank you for showing up',
+    'Your words are meaningful',
+    'I hear what you are saying',
+    'I appreciate your time',
+    'Thank you for sharing',
     'I care about you'
 ]
 
